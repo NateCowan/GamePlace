368581b54e0fcfd051a0540f73cbce69060ee3a1
Nate Cowan <ncowan0123@gmail.com>
2024-04-04 15:01:33 -0600

    added 2 tests for endpoints. One is for redirect, one is for render. These tests need to be tested.

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -73,3 +73,31 @@ describe('Testing register API', () => {
       });
   });
 });
+
+describe('Testing Redirect', () => {
+  // Sample test case given to test /test endpoint.
+  it('\login route should redirect to /login with 302 HTTP status code', done => {
+    chai
+      .request(server)
+      .get('/login')
+      .end((err, res) => {
+        res.should.have.status(302); // Expecting a redirect status code
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
+        done();
+      });
+  });
+});
+
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
