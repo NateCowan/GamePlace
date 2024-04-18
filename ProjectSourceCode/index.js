64b5c19a686e18146b6f63b1a094f185343abcf9
Sabrina Vu <savu2328@colorado.edu>
2024-04-18 12:13:58 -0600

    Follows route, success!

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -516,29 +516,6 @@ app.get('/game', async (req, res) => {
 });
 
 
-// app.post('/follow', async (req, res) => {
-//   try {
-//     const { userId, gameId, action } = req.body;
-//     // Perform authentication check here
-    
-//     if (action === 'follow') {
-//       // Add a record to the database to indicate that the user is following the game
-//       await db.query('INSERT INTO follows (user_id, game_id) VALUES ($1, $2)', [userId, gameId]);
-//       res.json({ success: true, message: 'Game followed successfully' });
-//       res.redirect('/account');
-//     } else if (action === 'unfollow') {
-//       // Remove the record from the database to indicate that the user is no longer following the game
-//       await db.query('DELETE FROM follows WHERE user_id = $1 AND game_id = $2', [userId, gameId]);
-//       res.json({ success: true, message: 'Game unfollowed successfully' });
-//     } else {
-//       res.status(400).json({ success: false, message: 'Invalid action' });
-//     }
-//   } catch (error) {
-//     console.error('Error handling follow/unfollow action:', error);
-//     res.status(500).json({ success: false, message: 'Internal Server Error' });
-//   }
-// });
-
 // Route to follow a game
 app.post('/game/follow', async (req, res) => {
   try {
